e619a434e416e87d9854ba84167f0f8e3739f383
Lawrencesalinas <lawrencesalinas@yahoo.com>
2021-10-29 04:35:11 -0400

    added scoring

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 const canvas = document.querySelector("canvas")
 canvas.height = window.innerHeight
 canvas.width = window.innerWidth
-
+const resetGame = document.getElementById("restart")
 const c = canvas.getContext("2d")
+let playerOneScore = 0;
+let playerTwoScore = 0
+let roundWiiner = ""
+let weHaveAwinner = false
 
 
-function Player(x, y , color, width, height){
+const images = document.getElementById("source")
+function Shield(image, x, y, width, height){
+    this.image = image
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.render = () =>{
+        c.drawImage(this.image, this.x, this.y, this.width, this. height )
+    }
+}
+
+function Player(x, y , color, width, height){             
     this.x = x
     this.y = y 
     this.width = width 
@@ -14,71 +30,68 @@ function Player(x, y , color, width, height){
     this.render = () => {
         c.fillStyle = this.color
         c.fillRect(this.x, this.y, this.width, this.height)
-
     }
 }
 
-function Circle(x, y, radius){
+function Circle(x, y, radius,dy, caught){
+    
     this.x = x
     this.y = y
-    this.radius = radius
+    this.radius = radius 
+    this.dy = dy
     this.draw = () =>{
         c.beginPath()
         c.arc(this.x , this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = " #FEDE17"
         c.stroke()
+        c.fill()
     }
-}
+    this.fire = () => {
+        this.y += this.dy 
+    }
+    this.detect = () => {
+        if(this.y - this.radius < player.y + player.height && this.x + this.radius > shield.x && this.x < shield.x + shield.width){
+            this.y = 300
+            this.x = -20
+            this.dy = 0
+            playerOneScore++       
+         }  
+         }
+    this.detectWall = () => {
+        if(this.y - this.radius < 0){
+            this.y = 300
+            this.x = -20
+            this.dy = 0
+            playerTwoScore++  
+        }
+    }
+    }    
+
 
 let canvasXmid = (canvas.width/2 - 100)
-let player = new Player(canvasXmid, 0, '#bada55', 200, 50)
-let player2 = new Player(canvasXmid, canvas.height - 50, "blue", 200, 50)
-let circle = new Circle(50, 0, 50,)
+let player = new Player(canvasXmid, 0, '#bada55', 130, 70)
+let player2 = new Player(canvasXmid, canvas.height - 50, "brown", 150, 40)
+let shield = new Shield(images,canvasXmid, 0, 140, 80)
+let circles = []
 
-
-// let circleBox = []
-// const createCircle = () => {
-//     let circle = new Circle('circle',50, 0, 10, 0)
-//     circleBox.push(circle)
-//     console.log(circleBox)
-//     for(let i = 0; i < circleBox.length; i++){
-//         circle = circleBox[i]
-//        }
-// }
-
-
-
-var controls = {}
-const controller = (key, keyDown) => {
-if(key == "d"){
-    controls.shooterRight = keyDown
+const fire = (e) => {
+     switch(e.key){
+        case " ":
+    let circle = new Circle(100 +player2.x, player2.y, 20,-7) 
+        circles.push(circle)  
+         }
 }
-if(key == "a"){
-    controls.shooterLeft = keyDown
-}
-if(key == "ArrowRight"){
-    controls.catcherRight = keyDown
-}
-if(key == "ArrowLeft"){
-    controls.catcherLeft = keyDown
-}
-if(key == " "){
-    controls.shooting = keyDown
-    // createCircle()
-}
-
-}
-
 const move = () => {
     if(controls.shooterRight){
-        player.x += 20
-        if(player.x + player.width >= canvas.width){
-            player.x = canvas.width - player.width
+        shield.x += 20
+        if(shield.x + shield.width >= canvas.width){
+            shield.x = canvas.width - shield.width
         }
     }
     if (controls.shooterLeft){
-        player.x -= 20
-        if(player.x < 0){
-            player.x = 0
+        shield.x -= 20
+        if(shield.x < 0){
+            shield.x = 0
         }
     }
     if(controls.catcherLeft){
@@ -94,43 +107,88 @@ const move = () => {
             player2.x = canvas.width - player2.width
         }
     }
-    if(controls.shooting){
-        circle.x = 100 + player2.x
-        circle.y = player2.y
-        circle.y -= 10
-        
-    //     createCircle()
-    //     for(let i = 0; i < circleBox.length; i++)
-    //     circleBox[i].draw()
+}
+    
+
+let controls = {}
+const controller = (key, keyDown) => {
+if(key == "d"){
+    controls.shooterRight = keyDown
+}
+if(key == "a"){
+    controls.shooterLeft = keyDown
+}
+if(key == "ArrowRight"){
+    controls.catcherRight = keyDown
+}
+if(key == "ArrowLeft"){
+    controls.catcherLeft = keyDown
+}
+}
+
+const gameReset = (a) => {
+  const reset = a.target.resetGame
+      playerOneScore = 0
+      playerTwoScore = 0
+      roundWiiner = ""
+      shield.x = canvasXmid
+      shield.y = 0
+      player2.x = canvasXmid
+      player2.y = canvas.height - 50
+      weHaveAwinner = false
+    
+      
+} 
+
+
+
+    
+const gerScoresAndResult = () => {
+    const scores = document.getElementById("score")
+    scores.innerText = `Player 1 score: ${playerOneScore} \n\n Player 2 score: ${playerTwoScore}`
+     if(playerOneScore === 20 && playerTwoScore < 20){
+            roundWiiner = "player1"
+            weHaveAwinner = true;
+     } else if(playerTwoScore === 20 && playerOneScore < 20 ){
+            roundWiiner = "player2"
+            weHaveAwinner = true;
     }
-}
+        
+    const result = document.getElementById("result")
+    result.innerText = `The round winner is ${roundWiiner}`
+    
+    }
 
-
-const shootCircle = () => {
-     circle.y -= 10
-}
-
+ 
 const clearInt = () =>{
     c.clearRect(0, 0, canvas.width, canvas.height)
 }
 
 const gameLoop = () => {
     
-    shootCircle()
     clearInt()
-    circle.draw()
-    player.render()
+    shield.render()
     player2.render()
+    for(var i = 0; i < circles.length; i++){
+        circles[i].draw()
+        circles[i].fire()
+        circles[i].detect()
+        circles[i].detectWall()
+     }
     move()
-
+    gerScoresAndResult()
+    
 }
 
 
 
 gameInterval = setInterval(gameLoop, 1000/60)
+let stopGameLoop = () => {clearInterval(gameInterval)}
 document.addEventListener("keydown", (e) =>{
     controller(e.key, true)
 })
 document.addEventListener("keyup", (e) => {
     controller(e.key, false)
 })
+restart.addEventListener("click", gameReset)
+document.addEventListener('keydown', fire)
