80597cfbafb045a2743267f19b416cf1408c43fd
Lawrencesalinas <lawrencesalinas@yahoo.com>
2021-12-31 18:05:45 -0500

    update

--- js/script.js
+++ js/script.js
@@ -194,6 +194,7 @@ const gameLoop = () => {
 
 setInterval(gameLoop,1000/60)
 
+
 /*adding eventListerners to the program*/
 document.addEventListener("keydown", (e) =>{
     controller(e.key, true)
@@ -203,8 +204,6 @@ document.addEventListener("keyup", (e) => {
 })
 
 restart.addEventListener("click", gameReset)
-<<<<<<< HEAD
+
 document.addEventListener('keydown', fire)
-=======
-document.addEventListener('keydown', fire)
->>>>>>> e300862518ad3e722c785c8eb2823a4f8252a573
+
